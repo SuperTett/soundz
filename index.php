eb6c7fb73272b20c23a949e10ffe1943e68eb567
SuperTett <simontett.web@gmail.com>
2020-02-20 16:55:50 -0500

    Added some things in index

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+@todo
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,6 +11,16 @@
 include ("snipets/header.html");
 ?>
 <body>
-
+<main id="home">
+    <h1>Home</h1>
+    <!-- This part would display the few new sounds uploaded to the site:
+        programming a date tracker with SQL -->
+    <h2>New addings</h2>
+    <ul>
+        <li><a href="#">A new SFX</a></li>
+        <li><a href="#">A new SFX</a></li>
+        <li><a href="#">A new SFX</a></li>
+    </ul>
+</main>
 </body>
 </html>
