984200f8e5a6f9a2c79fbca6804ef9f0ba24090a
SuperTett <simontett.web@gmail.com>
2020-02-22 15:34:31 -0500

    more work on axure sketch

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <title>Soundz - Home</title>
     <meta name="description" content="need to write a description in here">
-    <link type="text/css" rel="stylesheet" href="styles.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/styles.css">
 </head>
 <?php
 include ("snipets/header.html");
